a1cfc225e67d9a98d7280cae1c129682eb6f9f68
Ximenams <x.martinez.s@edu.utc.mx>
2023-02-09 15:00:34 -0600

    trabajos

---
+++ 05EjerciciosJS/JS/interest.js
@@ -0,0 +1,27 @@
+
+function validarn(e){
+    var  teclado  =  (document. all)? e. keyCode : e. which;
+
+    if(teclado == 8) return true;
+
+    var pattern = /[0-9\d .] /;
+
+    var code = String. fromCharCode(keyboard);
+
+    return  patron. test(codigo);
+}
+
+function interes(){
+    var value = document. form. quantity. value;
+    var resul = parseInt(valor);
+    var interest = result*0.02;
+    var total = interest * resul;
+
+    document. formulario. sueldoI. value=  "$"  +  total;
+}
+
+function borrardatos(){
+
+    document. form. quantity. value= "";
+    document. formulario. sueldoI. value= "";
+}
---
+++ tareavalidacion/JS/validacion.js
@@ -0,0 +1,83 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	
+}
+
+const campos = {
+	usuario: false,
+	nombre: false,
+	password: false,
+	correo: false,
+	telefono: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "usuario":
+			validarCampo(expresiones.usuario, e.target, 'usuario');
+		break;
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo_${campo}`).classList.remove('formulario_grupo-incorrecto');
+		document.getElementById(`grupo_${campo}`).classList.add('formulario_grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo_${campo} .formularioinput-error`).classList.remove('formulario_input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo_${campo}`).classList.add('formulario_grupo-incorrecto');
+		document.getElementById(`grupo_${campo}`).classList.remove('formulario_grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo_${campo} .formularioinput-error`).classList.add('formulario_input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo_password2`).classList.add('formulario_grupo-incorrecto');
+		document.getElementById(`grupo_password2`).classList.remove('formulario_grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo_password2 .formularioinput-error`).classList.add('formulario_input-error-activo');
+		campos['password'] = false;
+	} else {
+		document.getElementById(`grupo_password2`).classList.remove('formulario_grupo-incorrecto');
+		document.getElementById(`grupo_password2`).classList.add('formulario_grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo_password2 .formularioinput-error`).classList.remove('formulario_input-error-activo');
+		campos['password'] = true;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});

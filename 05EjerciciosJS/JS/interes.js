0895c5a52eb24ff9b91e2d6c37e5a286734f4eaf
Ximenams <x.martinez.s@edu.utc.mx>
2023-02-12 14:30:26 -0600

    formulario

--- 05EjerciciosJS/JS/interes.js
+++ 05EjerciciosJS/JS/interes.js
@@ -26,3 +26,16 @@ function borrardatos(){
     document.formulario.cantidad.value= "";
     document.formulario.sueldoI.value= "";
 }
+
+function comision(){
+    var sueldoBase;
+    var venta1;
+    var venta2;
+    var venta3;
+    var totalComision;
+    var totalPagar;
+
+    totalComision=(parseInt(venta1)+parseInt(venta2)+parseInt(venta3))*0.1;
+    totalPagar=parseInt(sueldoBase)+parseInt(totalComision);
+
+}
